cde1305953b9d73f873798f9fdd68941fd38d21f
daidr <daidr@daidr.me>
2021-03-07 07:54:28 +0800

    Upgrade dependency

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml');
 
 try {
     let configFile = fs.readFileSync('./config/config.yml', 'utf8');
-    let config = yaml.safeLoad(configFile);
+    let config = yaml.load(configFile);
     module.exports = config;
 } catch (e) {
     throw "Configuration file not found.";

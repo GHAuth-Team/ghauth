f21877a9613415c4962fb458326de9ef81850849
daidr <daidr@daidr.me>
2022-06-21 14:44:29 +0800

    fix(adminList): set default value if no adminList.yml found

--- adminList.js
+++ adminList.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml');
 
 try {
   const configFile = fs.readFileSync('./config/adminList.yml', 'utf8');
-  const config = yaml.load(configFile);
+  const config = yaml.load(configFile) || [];
   module.exports = config;
 } catch (e) {
   throw new Error('AdminList file not found.');

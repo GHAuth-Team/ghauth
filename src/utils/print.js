d290e0b9c81288186d9e58712f89b20ac7f23245
daidr <daidr@daidr.me>
2022-07-20 19:42:08 +0800

    feat: add debug print methods

--- src/utils/print.js
+++ src/utils/print.js
@@ -2,15 +2,37 @@ const color = require('colors/safe')
 
 module.exports = {
   info(...args) {
-    console.log(color.bold(color.blue(`[信息 | ${process.pid}] `)), ...args)
+    console.log(color.blue.bold(`[信息] `), ...args)
   },
   success(...args) {
-    console.log(color.bold(color.green(`[成功 | ${process.pid}] `)), ...args)
+    console.log(color.green.bold(`[成功] `), ...args)
   },
   warn(...args) {
-    console.log(color.bold(color.yellow(`[警告 | ${process.pid}] `)), ...args)
+    console.log(color.yellow.bold(`[警告] `), ...args)
   },
   error(...args) {
-    console.log(color.bold(color.red(`[错误 | ${process.pid}] `)), ...args)
+    console.log(color.red.bold(`[错误] `), ...args)
+  },
+  debug: {
+    info(...args) {
+      if (process.env.NODE_ENV === 'development') {
+        console.log(color.blue.bold(`[调试][信息] `), ...args)
+      }
+    },
+    success(...args) {
+      if (process.env.NODE_ENV === 'development') {
+        console.log(color.green.bold(`[调试][成功] `), ...args)
+      }
+    },
+    warn(...args) {
+      if (process.env.NODE_ENV === 'development') {
+        console.log(color.yellow.bold(`[调试][警告] `), ...args)
+      }
+    },
+    error(...args) {
+      if (process.env.NODE_ENV === 'development') {
+        console.log(color.red.bold(`[调试][错误] `), ...args)
+      }
+    }
   }
 }
